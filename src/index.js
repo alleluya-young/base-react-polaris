73ad5b4307f59d14d3a37b93751301bb98cda46d
智慧哥哥 <alleluya7519@163.com>
2021-02-05 21:59:37 +0800

    feat: synchronize from source

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { AppProvider } from "@shopify/polaris";
-import "@shopify/polaris/dist/styles.css";
-import zhTranslations from "@shopify/polaris/locales/zh-CN.json";
 import { Global } from "@emotion/react";
 import { normalize } from "polished";
 import { conf, ConfigProvider } from "./core/config";
@@ -11,12 +8,10 @@ import { Routes } from "./routes";
 
 ReactDOM.render(
   <React.StrictMode>
-    <AppProvider i18n={zhTranslations}>
-      <ConfigProvider value={{ config: conf() }}>
-        <Global styles={normalize()} />
-        <Routes />
-      </ConfigProvider>
-    </AppProvider>
+    <ConfigProvider value={{ config: conf() }}>
+      <Global styles={normalize()} />
+      <Routes />
+    </ConfigProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
